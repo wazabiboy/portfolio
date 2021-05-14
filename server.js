01f2b03d80f752e0f5add72a29f7a20644e1edfe
wazabyboy <wazabi.boy@gmail.com>
2021-05-14 22:24:34 +0200

    cc

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./src/app.component.html'));
+
+app.get('/*', function (req, res) {
+  res.sendFile('app.component.html', { root: 'src/app.component.html' }
+  );
+});
+
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
